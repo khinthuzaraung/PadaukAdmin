50db7b3d49248fd9f12d8a899a56996297dc99d9
zinmon <zinmonthet@myanmar.gnext.asia>
2017-09-15 09:22:29 +0630

    minko

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,70 @@
 |
 */
 
+Route::get('404', function () {
+    return view('404');
+});
+
+Route::get('blank', function () {
+    return view('blank');
+});
+
+Route::get('buttons', function () {
+    return view('buttons');
+});
+
+Route::get('charts', function () {
+    return view('charts');
+});
+
+Route::get('grids', function () {
+    return view('grids');
+});
+
+Route::get('icons', function () {
+    return view('icons');
+});
+
+Route::get('inbox-details', function () {
+    return view('inbox-details');
+});
+
+Route::get('inbox', function () {
+    return view('inbox');
+});
+
+Route::get('login', function () {
+    return view('login');
+});
+
+Route::get('maps', function () {
+    return view('maps');
+});
+
+Route::get('portlet', function () {
+    return view('portlet');
+});
+
+Route::get('price', function () {
+    return view('price');
+});
+
+Route::get('product', function () {
+    return view('product');
+});
+
+Route::get('signup', function () {
+    return view('signup');
+});
+
+Route::get('typography', function () {
+    return view('typography');
+});
+
+Route::get('index', function () {
+    return view('index');
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
